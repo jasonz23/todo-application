784fa7608b87a72fffd29feb653b1d62a51b86ff
Jason <jason1382311@gmail.com>
2022-03-08 13:36:49 -0800

    fix issue with undoing completed tasks

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ import ToDoForm from './ToDoForm';
 
 function App() {
   const [ toDoList, setToDoList] = useState(data);
+
   const handleToggle = (id) => {
     let mapped = toDoList.map(task => {
-      return task.id == id? { ...task, complete: !toHaveStyle.complete}: {...task};
+      return task.id == Number(id) ? { ...task, complete: !task.complete}: {...task};
     });
     setToDoList(mapped);
   }
@@ -27,7 +28,7 @@ function App() {
    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
    setToDoList(copy);
  }
- 
+
   return (
     <div className="App">
       <Header />  
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
 
 const ToDo = ({todo, handleToggle}) => {
+
     const handleClick = (e) => {
         e.preventDefault()
         handleToggle(e.currentTarget.id)
     }
+
     return (
         <div id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} onClick={handleClick} className={todo.complete ? "todo strike" : "todo"}>
             {todo.task}
