e00cdbac523e605c6d4f162f3f434b58f07218ab
Jason <jason1382311@gmail.com>
2022-03-08 13:27:11 -0800

    added code

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import React, {useState} from "react";
 import data from "./data.json";
 import Header from "./Header";
 import ToDoList from "./ToDoList";
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 
 function App() {
   const [ toDoList, setToDoList] = useState(data);
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id == id? { ...task, complete: !toHaveStyle.complete}: {...task};
+    });
+    setToDoList(mapped);
+  }
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
   return (
     <div className="App">
       <Header />  
+      <ToDoList toDoList={toDoList} handleFilter = {handleFilter} handleToggle = {handleToggle} />
     </div>
   );
 }
