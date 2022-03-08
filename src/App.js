99fa8f00a5293dfe435cd04a001920c3efa10f05
Jason <jason1382311@gmail.com>
2022-03-08 13:29:26 -0800

    allow to add tasks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import data from "./data.json";
 import Header from "./Header";
 import ToDoList from "./ToDoList";
 import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
+import ToDoForm from './ToDoForm';
 
 function App() {
   const [ toDoList, setToDoList] = useState(data);
@@ -20,10 +21,18 @@ function App() {
     });
     setToDoList(filtered);
   }
+
+   const addTask = (userInput) => {
+   let copy = [...toDoList];
+   copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+   setToDoList(copy);
+ }
+ 
   return (
     <div className="App">
       <Header />  
       <ToDoList toDoList={toDoList} handleFilter = {handleFilter} handleToggle = {handleToggle} />
+      <ToDoForm addTask = {addTask} />
     </div>
   );
 }
